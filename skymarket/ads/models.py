672ba6a0d2b8d0b9b834539d00b362c4afccb77f
Timofey <tima.khlystov99@mail.ru>
2024-03-29 06:39:38 +0300

    Add models

--- skymarket/ads/models.py
+++ skymarket/ads/models.py
@@ -1,12 +1,35 @@
 from django.conf import settings
 from django.db import models
 
+NULLABLE = {"null": True, "blank": True}
 
 class Ad(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+    title = models.CharField(max_length=100, verbose_name="Название товара")
+    price = models.PositiveIntegerField(verbose_name="Цена товара")
+    description = models.TextField(verbose_name="Описание товара")
+    image = models.ImageField(upload_to='media', **NULLABLE, verbose_name="Фото")
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Пользователь",
+                               **NULLABLE)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
 
+    def __str__(self):
+        return f"{self.title} - {self.price}"
+
+    class Meta:
+        verbose_name = "объявление"
+        verbose_name_plural = "объявления"
+        ordering = ["-created_at"]
 
 class Comment(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+    text = models.TextField(verbose_name="Текст отзыва", null=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Пользователь",
+                               **NULLABLE)
+    ad = models.ForeignKey(Ad, on_delete=models.CASCADE, verbose_name="Объявление")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = "отзыв"
+        verbose_name_plural = "отзывы"
--- skymarket/users/models.py
+++ skymarket/users/models.py
@@ -4,13 +4,45 @@ from users.managers import UserManager
 from phonenumber_field.modelfields import PhoneNumberField
 from django.utils.translation import gettext_lazy as _
 
+NULLABLE = {"null": True, "blank": True}
 
-class UserRoles:
-    # TODO закончите enum-класс для пользователя
-    pass
+
+class UserRoles(models.TextChoices):
+    ADMIN = "Admin"
+    USER = "User"
 
 
 class User(AbstractBaseUser):
-    # TODO переопределение пользователя.
-    # TODO подробности также можно поискать в рекоммендациях к проекту
-    pass
+    username = None
+    first_name = models.CharField(max_length=50, verbose_name='Имя пользователя')
+    last_name = models.CharField(max_length=50, verbose_name='Фамилия пользователя')
+    phone = models.CharField(max_length=15, verbose_name='Телефон пользователя')
+    email = models.EmailField(unique=True, verbose_name='Почта пользователя')
+    role = models.CharField(max_length=10, default=UserRoles.USER, choices=UserRoles.choices, verbose_name="Роль пользователя")
+    image = models.ImageField(upload_to='profile_images', verbose_name='Аватарка пользователя', **NULLABLE)
+    is_active = models.BooleanField(default=False, verbose_name='Активность пользователя')
+
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone']
+
+    def is_superuser(self):
+        return self.is_admin
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return self.is_admin
+
+    @property
+    def is_admin(self):
+        return self.role == UserRoles.ADMIN
+
+    @property
+    def is_user(self):
+        return self.role == UserRoles.USER
