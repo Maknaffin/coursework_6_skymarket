8ab395efd448a2339ead840df3c8d4787da4aa46
Timofey <tima.khlystov99@mail.ru>
2024-03-29 06:42:19 +0300

    Add urls for ads and comments

--- skymarket/ads/urls.py
+++ skymarket/ads/urls.py
@@ -1,8 +1,17 @@
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from .apps import SalesConfig
+from .views import AdViewSet, CommentViewSet, AdMyListAPIView
+from rest_framework_nested.routers import NestedDefaultRouter
 
-# TODO настройка роутов для модели
+app_name = SalesConfig.name
 
+ad_router = DefaultRouter()
+ad_router.register(r'ads', AdViewSet, basename='Объявление')
+
+com_router = NestedDefaultRouter(ad_router, r'ads', lookup='ad')
+com_router.register(r'comments', CommentViewSet, basename='Комментарий')
 
 urlpatterns = [
-
-]
+    path('ads/me/', AdMyListAPIView.as_view(), name='Мои объявления')
+] + ad_router.urls + com_router.urls
