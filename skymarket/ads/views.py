0439581628d6c175a7e2aafcea944853cf6a30fa
Timofey <tima.khlystov99@mail.ru>
2024-03-29 06:43:02 +0300

    Add CRUD for ads and comments

--- skymarket/ads/views.py
+++ skymarket/ads/views.py
@@ -1,15 +1,74 @@
-from rest_framework import pagination, viewsets
+from rest_framework import pagination, viewsets, generics
+
+from .filters import AdFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Ad, Comment
+from .permissions import IsOwnerOrAdmin
+from .serializers import AdSerializer, AdListSerializer, CommentListSerializer, CommentCreateSerializer
 
 
 class AdPagination(pagination.PageNumberPagination):
-    pass
+    page_size = 4
 
 
-# TODO view функции. Предлагаем Вам следующую структуру - но Вы всегда можете использовать свою
 class AdViewSet(viewsets.ModelViewSet):
-    pass
+    serializer_class = AdSerializer
+    queryset = Ad.objects.all()
+    pagination_class = AdPagination
+    permission_classes = [IsAuthenticated]
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = AdFilter
+
+    def get_serializer_class(self, *args, **kwargs):
+        if self.action == "retrieve":
+            return AdListSerializer
+        else:
+            return AdSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def get_permissions(self):
+        if self.action == "list":
+            self.permission_classes = []
+        elif self.action in ['create', 'retrieve']:
+            self.permission_classes = [IsAuthenticated]
+        elif self.action in ['update', 'partial_update', 'destroy']:
+            self.permission_classes = [IsAuthenticated, IsOwnerOrAdmin]
+        return super().get_permissions()
+
+
+class AdMyListAPIView(generics.ListAPIView):
+    serializer_class = AdSerializer
+    queryset = Ad.objects.all()
+    pagination_class = AdPagination
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Ad.objects.filter(author=self.request.user)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
-    pass
+    serializer_class = CommentListSerializer
+    queryset = Comment.objects.all()
+    permission_classes = [IsAuthenticated]
 
+    def get_serializer_class(self, *args, **kwargs):
+        if self.action == "create":
+            return CommentCreateSerializer
+        return CommentListSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user, ad_id=self.kwargs['ad_pk'])
+
+    def get_queryset(self):
+        return Comment.objects.filter(ad_id=self.kwargs['ad_pk'])
+
+    def get_permissions(self):
+        if self.action in ['list', 'create', 'retrieve']:
+            self.permission_classes = [IsAuthenticated]
+        elif self.action in ['update', 'partial_update', 'destroy']:
+            self.permission_classes = [IsAuthenticated, IsOwnerOrAdmin]
+        return super().get_permissions()
